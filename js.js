59b23a9668325365bfd21339682173f384f93eea
jediel <jediel-felipe@hotmail.com>
2020-12-26 18:06:28 -0300

    ajuste mobile first

--- js.js
+++ js.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("dday").innerHTML = "SITE NA TEIA";
+    document.getElementById("dday").innerHTML = "Evento Ocorrido";
   }
 }, 1000);
